6d3154d3680e299606a1c31c2c94ee20bd1ddc7c
andrew <andrewfok007@gmail.com>
2017-07-17 08:27:45 +0800

    updated lval_func with macros

--- lval_func.h
+++ lval_func.h
@@ -1,6 +1,9 @@
 #ifndef lval_func_h
 #define lval_func_h
 
+#define LASSERT(args, cond, err) \
+  if (!(cond)) { lval_del(args); return lval_err(err); }
+
 #include "lval.h"
 #include "lval_types.h"
 #include <stdlib.h>
@@ -29,5 +32,19 @@ lval* builtin_op(lval* a, char* op);
 lval* lval_eval(lval* v);
 
 lval* lval_eval_sexpr(lval* v);
+
+lval* builtin_head(lval* a);
+
+lval* builtin_tail(lval* a);
+
+lval* builtin_list(lval* a);
+
+lval* builtin_eval(lval* a);
+
+lval* builtin_join(lval* a);
+
+lval* lval_join(lval* x, lval* y);
+
+lval* builtin(lval* a, char* func);
 
 #endif
